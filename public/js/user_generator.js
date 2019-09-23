4415fbf19d466af3db0a58f946ca65f4ffd30f50
cadeborn <54521537+cadeborn@users.noreply.github.com>
2019-09-22 21:25:39 -0400

    Adjustment across repositories
    So, apparently I have been operating out of two different repositories under the same name (one being cadeborn/using-docker-cadeborn, the other being iu-msis/"..."). This update will make the two the same in all functional matters and should fix my docker problems.

--- public/js/user_generator.js
+++ public/js/user_generator.js
@@ -1,26 +1,48 @@
-var newUser = new Vue({
-el: #'superNewUser',
-data: {
-  user: [
-    {
-      "name"": "Mark Stevens",
-      "countryOfOrigin"": "United Kingdom",
-      "birthdate": "07/03/1995",
-      "age": "24",
-      "email": "mailto:madeupemail@gmail.com",
-      priority: "high"
-    },
-  ]
+// This is my JS, which uses an empty data structure based on the randomuser.me api structure
+var user_app = new Vue({
+  el: '#user_generator_app',
+  data: {
+    users: {
+
+      "name": {
+        "title": "",
+        "first": "",
+        "last": ""
+      },
+
+      "email": "",
+      "dob": {
+        "date": "",
+        "age": ""
+      },
+
+      "id": {
+        "name": "",
+        "value": ""
+      },
+
+      "picture": {
+        "large": "",
+        "medium": "",
+        "small": "",
+        "thumbnail": ""
+      },
+      "nat": "",
+
+    }
+  },
+// This is the method detailed in class and in the video which allows us to fetch a user from randomuser.me
+  methods: {
+    fetchUser() {
+      fetch('https://randomuser.me/api/')
+      .then(response => response.json())
+      .then(json => {user_app.users = json.results[0]});
+
+    }
+    
 },
-methods: {
-  fetchUser() {
-    fetch('https://randomuser.me/api/')
-    .then(function(response) {console.log(response.json() ))})
-  }
-},
-created() {
-  this.fetchUser()
+created: function() {
+  this.fetchUser();
 }
 
-}
-});
+})
